31faaea68cef4bb47df5ec2752e37f47f2177021
Tariq Samir <liliantarek4@gmail.com>
2024-09-04 22:00:00 +0300

    videos

---
+++ learning.js
@@ -0,0 +1,61 @@
+// start task 1
+// Replace ? With Arithmetic Operators
+console.log(10 * 20 * 15 % 3 * 190 * 10 * 400); // 0
+// end task 1
+// start task 2
+
+var num = 3;
+
+// Solution One
+console.log(num+num); // 6
+
+// Solution Two
+console.log(num*--num); // 6
+
+// Soultion Three
+console.log(num+num+num);
+
+// Soultion Four
+console.log(num*++num); // 6
+
+
+
+// Solution Five
+
+console.log(num++ + --num);//6 
+
+// Solution Six
+console.log(num**num/num-num); // 6
+
+
+
+// end task 2
+// start task 3
+var num = "10";
+
+// Solution One
+console.log(+num + +num); // 20
+
+// Solution Two
+console.log(--num + ++num +true); // 20
+
+// Solution Three
+console.log(num-- + ++num); // 20
+
+// Solution Four
+console.log(num+=num); // 20
+
+
+// end task 3
+// start task 4
+let points = 10;
+
+points+=3;
+
+console.log(points); // 13
+
+points-=5;
+
+console.log(points); // 8;
+
+// end task 4
